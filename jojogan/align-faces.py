3f79a0f1ec9ee23096fe39b71f4e8ec42fcfb6fb
Giorgia Bortoletti <56470846+bortoletti-giorgia@users.noreply.github.com>
2023-03-29 16:22:47 +0200

    fix: minor fixes on align-faces.py

--- jojogan/align-faces.py
+++ jojogan/align-faces.py
@@ -1,4 +1,3 @@
-import torch
 from torchvision import transforms, utils
 from PIL import Image
 import math
@@ -9,33 +8,66 @@ import numpy as np
 from torch import nn, autograd, optim
 from torch.nn import functional as F
 from tqdm import tqdm
-import wandb
+import argparse
 
+# JoJoGAN libraries
 from model import *
 from e4e_projection import projection as e4e_projection
 from util import *
 
-folder = '/home/bortoletti/rosbag/'
-subfolders = [ f.path for f in os.scandir(folder) if f.is_dir() ]
+# Arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--inputdir", help="directory where images are saved", required=True)
+parser.add_argument("--outputdir", help="directory where aligned images will be saved", required=True)
 
-outdir = '/home/bortoletti/rosbag/aligned-faces/'
+args = parser.parse_args()
 
-if not os.path.exists(outdir):
-	os.mkdir(outdir)
+inputdir = args.inputdir
+outputdir = args.outputdir
+
+if not os.path.exists(outputdir):
+	os.mkdir(outputdir)
 
 device = 'cuda'
 
-for i, subfolder in enumerate(subfolders):
-    if subfolder.split('/')[-1][0:4] == '2023':
-        files = os.listdir(subfolder)
-        for filename in files:
-            if filename[-4:] == '.jpg': # image
-                filepath = os.path.join(subfolder, filename)
+no_face_detected = 0
+
+if "rosbag" in inputdir:
+    subfolders = [f.path for f in os.scandir(inputdir) if f.is_dir() ]
+    # read every rosbag folder
+    for i, subfolder in enumerate(subfolders):
+        if subfolder.split('/')[-1][0:4] == '2023':
+            files = os.listdir(subfolder)
+            for filename in files:
+                if filename[-4:] == '.jpg': # image
+                    filepath = os.path.join(subfolder, filename)
+                    name = strip_path_extension(filepath)+'.pt'
+                    # aligns and crops face from the rosbags image
+                    try:
+                        aligned_face = align_face(filepath, output_size=256, transform_size=256)
+                        my_w = e4e_projection(aligned_face, name, device).unsqueeze(0)
+                        finaldir = os.path.join(outputdir, subfolder.split('/')[-1])
+                        if not os.path.exists(finaldir):
+                            os.mkdir(finaldir)
+                        plt.imsave(os.path.join(finaldir, filename), get_image(aligned_face))
+                    except AssertionError: # no face detected
+                        pass
+else: # folder of images
+    files = os.listdir(inputdir)
+    for filename in files:
+        if filename[-4:] == '.jpg': # image
+            filepath = os.path.join(inputdir, filename)
+            if not os.path.exists(os.path.join(outputdir, filename)):
                 name = strip_path_extension(filepath)+'.pt'
                 # aligns and crops face from the rosbags image
                 try:
                     aligned_face = align_face(filepath, output_size=256, transform_size=256)
                     my_w = e4e_projection(aligned_face, name, device).unsqueeze(0)
-                    plt.imsave(os.path.join(outdir, str(i)+'_'+filename), get_image(aligned_face))
-                except AssertionError: # no face detected
-                    pass
+                    plt.imsave(os.path.join(outputdir, filename), get_image(aligned_face))
+                except: # no face detected (AssertionError, RuntimeError, ValueError)
+                    print("No face detected")
+                    no_face_detected += 1
+                    #pass
+
+print("No face detected in "+str(no_face_detected)+" images.")
+    
