f5c9a1eb152756591a32807aa6ab8859f2052144
Giorgia Bortoletti <56470846+bortoletti-giorgia@users.noreply.github.com>
2023-03-29 14:29:41 +0200

    feat: add analysis for UTKFace into-the-wild

--- dataset-analysis/create-dataset-folders.py
+++ dataset-analysis/create-dataset-folders.py
@@ -2,43 +2,80 @@ import argparse
 import pandas as pd
 import os
 import cv2
+from age_groups import *
 
 # SET CSV folder, CSV filename and dataset name
 parser = argparse.ArgumentParser()
 parser.add_argument("--outdirgender", help="path output folders based on gender")
 parser.add_argument("--outdirage", help="path output folders based on gender")
-parser.add_argument("--filename", help="CSV dataset filename")
-parser.add_argument("--source", help="path where CSV file is saved")
+parser.add_argument("--filename", help="CSV dataset filename", required=True)
+parser.add_argument("--source", help="path where CSV file is saved", required=True)
+parser.add_argument("--nsamples", help="number of sample to keep for each label", type=int, required=False)
 
 args = parser.parse_args()
 
 csv_folder = args.source
-csv_path = csv_folder + args.filename
+csv_path = os.path.join(csv_folder, args.filename)
 outdir_gender = args.outdirgender
-dir_age_path = args.outdirage
+outdir_age = args.outdirage
+num_sample_range = args.nsamples
 
 df = pd.read_csv(csv_path)
 n_tot_images = df.shape[0]
 
-# Create Gender and Age folders
-os.mkdir(outdir_gender)
-dir_female_path = outdir_gender + "1"
-dir_male_path = outdir_gender + "0"
-os.mkdir(dir_female_path)
-os.mkdir(dir_male_path)
-os.mkdir(dir_age_path)
+take_all_samples = False
+if num_sample_range is None:
+    take_all_samples = True
 
-for i in range(n_tot_images):
-    sample = df.loc[i]
-    img = cv2.imread(sample.filepath)
-    # gender
-    if sample.gender == "female":
-        cv2.imwrite(dir_female_path+"/"+sample.filename, img)
-    elif sample.gender == "male":
-        cv2.imwrite(dir_male_path+"/"+sample.filename, img)
-    # age
-    label = sample['age-group']
-    dir_label_path = dir_age_path+str(label)
-    if not os.path.isdir(dir_label_path):
-        os.mkdir(dir_label_path)
-    cv2.imwrite(dir_label_path+"/"+sample.filename, img)
+# Create Gender folders
+if outdir_gender is not None:
+    if not os.path.exists(outdir_gender):
+        os.mkdir(outdir_gender)
+
+    # female
+    dir_female_path = os.path.join(outdir_gender, "1")
+    if not os.path.exists(dir_female_path):
+        os.mkdir(dir_female_path)
+    df_female = df[df['gender'] == 'female']
+
+    if take_all_samples:
+        num_sample_range = len(df_female)
+
+    for i in range(num_sample_range):
+        img = cv2.imread(df_female.iloc[i].filepath)
+        name = df_female.iloc[i].filename
+        cv2.imwrite(dir_female_path+"/"+name, img)
+
+    # male
+    dir_male_path = os.path.join(outdir_gender, "0")
+    if not os.path.exists(dir_male_path):
+        os.mkdir(dir_male_path)
+    df_male = df[df['gender'] == 'male']
+
+    if take_all_samples:
+        num_sample_range = len(df_male)
+
+    for i in range(num_sample_range):
+        img = cv2.imread(df_male.iloc[i].filepath)
+        name = df_male.iloc[i].filename
+        cv2.imwrite(dir_male_path+"/"+name, img)
+
+# Create Age folders
+if outdir_age is not None:
+    if not os.path.exists(outdir_age):
+        os.mkdir(outdir_age)
+    ranges = AgeGroups().getRanges()
+
+    for i in range(len(ranges)):
+        dir_range = os.path.join(outdir_age, str(i))
+        if not os.path.exists(dir_range):
+            os.mkdir(dir_range)
+        df_i = df[df['age-group'] == i]
+
+        if take_all_samples:
+            num_sample_range = df_i.shape[0]
+
+        for j in range(num_sample_range):
+            img = cv2.imread(df_i.iloc[j].filepath)
+            name = df_i.iloc[j].filename
+            cv2.imwrite(dir_range+"/"+name, img)
